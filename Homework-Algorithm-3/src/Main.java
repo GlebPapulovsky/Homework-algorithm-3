d2668841138af3766441b0b8c2ed4da6211e87f6
gleb_papulovsky <papulovskijglech@gmail.com>
2024-06-20 03:31:41 +0300

    quit sort done

--- Homework-Algorithm-3/src/Main.java
+++ Homework-Algorithm-3/src/Main.java
@@ -1,3 +1,4 @@
+
 import java.io.*;
 import java.util.Arrays;
 
@@ -5,24 +6,57 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         int[][] teams = {
-                { 45, 31, 24, 22, 20, 17, 14, 13, 12, 10 },
-                { 31, 18, 15, 12, 10, 8, 6, 4, 2, 1 },
-                { 51, 30, 10, 9, 8, 7, 6, 5, 2, 1 }
+            {45, 31, 24, 22, 20, 17, 14, 13, 12, 10},
+            {31, 18, 15, 12, 10, 8, 6, 4, 2, 1},
+            {51, 30, 10, 9, 8, 7, 6, 5, 2, 1}
         };
 
         int[] nationalTeam = mergeAll(teams);
         System.out.println(Arrays.toString(nationalTeam)); // [51, 45, 31, 31, 30, 24, 22, 20, 18, 17]
     }
 
-    /** Метод для слияния всех команд в одну национальную */
+    /**
+     * Метод для слияния всех команд в одну национальную
+     */
     public static int[] mergeAll(int[][] teams) {
-        int[] mas={1,2,3};
-        return mas;
+        int[] top10 = teams[0];
+        for (int i = 0; i < teams.length - 1; i++) {
+            top10 = merge(top10, teams[i + 1]);
+        }
+
+        return top10;
     }
 
-    /** Метод для слияния двух команд в одну */
+    /**
+     * Метод для слияния двух команд в одну
+     */
     public static int[] merge(int[] teamA, int[] teamB) {
-        int[] mas={1,2,3};
-        return mas;
+        int[] top10 = new int[10];
+        int Acounter = 0;
+        int Bcounter = 0;
+        for (int i = 0; i < 10; i++) {
+            if (teamA[Acounter] > teamB[Bcounter]) {
+                top10[i] = teamA[Acounter];
+                Acounter++;
+            } else if (teamA[Acounter] < teamB[Bcounter]) {
+                top10[i] = teamB[Bcounter];
+                Bcounter++;
+            } else {
+                top10[i] = teamA[Acounter];
+                i++;
+                if (i == 10) {
+                    break;
+                
+                }  
+                top10[i]=teamB[Bcounter];
+                Acounter++;
+                Bcounter++;
+            }
+        }
+        return top10;
     }
+
+
+ 
+   
 }
