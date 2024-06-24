91bc0603e4e865d88b261cbdbf96d2e951c74982
gleb_papulovsky <papulovskijglech@gmail.com>
2024-06-25 02:25:54 +0300

    Change merge Function

--- Homework-Algorithm-3/src/Main.java
+++ Homework-Algorithm-3/src/Main.java
@@ -1,10 +1,9 @@
 
-import java.io.IOException;
 import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         int[][] teams = {
             {45, 31, 24, 22, 20, 17, 14, 13, 12, 10},
             {31, 18, 15, 12, 10, 8, 6, 4, 2, 1},
@@ -13,8 +12,7 @@ public class Main {
 
         int[] nationalTeam = mergeAll(teams);
         System.out.println(Arrays.toString(nationalTeam)); // [51, 45, 31, 31, 30, 24, 22, 20, 18, 17]
-        //test another sorts
-        
+
     }
 
     /**
@@ -33,62 +31,24 @@ public class Main {
      */
     public static int[] merge(int[] teamA, int[] teamB) {
         int[] top10 = new int[10];
-        int Acounter = 0;
-        int Bcounter = 0;
-        for (int i = 0; i < 10; i++) {
-            if (teamA[Acounter] > teamB[Bcounter]) {
-                top10[i] = teamA[Acounter];
-                Acounter++;
-            } else if (teamA[Acounter] < teamB[Bcounter]) {
-                top10[i] = teamB[Bcounter];
-                Bcounter++;
-            } else {
-                top10[i] = teamA[Acounter];
-                i++;
-                if (i == 10) {
-                    break;
+        int aCounter = 0;
+        int bCounter = 0;
+        int counter = 0;
+        while (true) {
 
-                }
-                top10[i] = teamB[Bcounter];
-                Acounter++;
-                Bcounter++;
+            if (teamA[aCounter] >= teamB[bCounter]) {
+                top10[counter] = teamA[aCounter];
+                aCounter++;
+                counter++;
+            } else if (teamA[aCounter] < teamB[bCounter]) {
+                top10[counter] = teamB[bCounter];
+                bCounter++;
+                counter++;
+            }
+            if (aCounter + bCounter == 10) {
+                break;
             }
         }
         return top10;
     }
-
-    //quick sort test
-    // public static int[] quickSort(int[] mas, int leftIndex, int rightIndex) {
-    //     //число для пивотирования
-    //     int pivot = mas[(leftIndex + rightIndex) / 2];
-    //     //маркеры(границы)пивотирования
-    //     int leftMarkerIndex = leftIndex;
-    //     int rightMarkerIndex = rightIndex;
-
-    //     while (leftMarkerIndex < rightMarkerIndex) {
-    //         while (mas[leftMarkerIndex] < pivot) {
-    //             leftMarkerIndex++;
-    //         }
-    //         while (mas[rightMarkerIndex] > pivot) {
-    //             rightMarkerIndex--;
-    //         }
-    //         if (leftMarkerIndex <= rightMarkerIndex) {
-    //             int swap = mas[leftMarkerIndex];
-    //             mas[leftMarkerIndex] = mas[rightMarkerIndex];
-    //             mas[rightMarkerIndex] = swap;
-    //             leftMarkerIndex++;
-    //             rightMarkerIndex--;
-    //         }
-
-    //     }
-    //     if (leftIndex < rightMarkerIndex) {
-    //         quickSort(mas, leftIndex, rightMarkerIndex);
-    //     }
-    //     if (rightIndex > leftMarkerIndex) {
-    //         quickSort(mas, leftMarkerIndex, rightIndex);
-    //     }
-    //     return mas;
-
-    // }
-
 }
